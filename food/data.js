83a09e8967891846e206e3098bc172dbe3999194
Natetorn <lsl3asic13@gmail.com>
2020-09-16 15:26:16 +0700

    restaurants07

---
+++ food/data.js
@@ -0,0 +1,57 @@
+const data = [
+ {
+    id:1,
+    name:'BonChon (บอนชอน) - ท่ามหาราช',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000021a/hero/6322e6fbb08a4002a6ca8ba998941a8a_1598580728800966917.png',
+    type:'Partner, Fried Chicken'
+},
+{
+    id:2,
+    name:'McDonalds (แมคโดนัลด์) ราชดำเนิน',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/AWh64KYIZXYdMpch2Zem/hero/91b61aa7c36445a19084dc93c77ea4c8_1600008272646540212.jpeg    ',
+    type:'Partner, Burgers, Fast Food, Fried Chicken'
+},
+{
+    id:3,
+    name:'Potato Corner (โปเตโต้ คอร์เนอร์) - ถนนข้าวสาร',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLJEVDYC3JFL6/hero/182731a9214e4cb793fc6142442013ba_1600055633637784048.jpeg',
+    type:'Partner, Small Bites/Snacks'
+},
+{
+    id:4,
+    name:'ตี๋เย็นตาโฟรสเด็ด - ศาลาว่าการกรุงเทพฯ',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMTKBDTT2XJAJ/hero/65641d0fcdc14d4a8961f56f0ead6fa5_1598847900883974941.jpeg',
+    type:'Partner, Noodles'
+},
+{
+    id:5,
+    name:'Kamu Tea (คามุ ที) - วังหลัง',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZC1VN5CBEXGAT/hero/2cac19cd03f84037a8046cd3d9a30b85_1585793335979199515.jpeg',
+    type:'Partner, Bubble Tea'
+},
+{
+    id:6,
+    name:'บุงกี่ เป็ดย่างฮ่องกง - บางลำพู',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4CJ2WAKAAZFE/hero/eac950d310ee457685bb98acf537d7ac_1576573337301905327.jpeg',
+    type:'Coupon, Partner, Chinese'
+},
+{
+    id:7,
+    name:'ถิงถิง บิงซูน้ำขิง - ยศเส',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY2BALJXE22ENJ/hero/a1852905efe244c48e7e47dfc7f5d97a_1598796718910246860.jpeg',
+    type:'Partner, Shaved Ice'
+},
+{
+    id:8,
+    name:'Inthanin Coffee (อินทนิล คอฟฟี่) - วิทยาลัยเพาะช่าง',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCVT3DWC4EWVX/hero/8c8ac7cfde1c4e49af9f78c4a280366c_1600006328576524547.jpg',
+    type:'Partner, Coffee'
+},
+{
+    id:9,
+    name:'ลาบเป็ดอุบล - บางขุนนนท์',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000008et/hero/db11b40b2782422b868fea5da7ec154d_1584504458628563773.jpeg',
+    type:'Coupon,Partner,Esan'
+}
+    ];
+    module.exports = data;
---
+++ food/routes/restaurants.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const restaurants = require('../data');
+
+router.get('/',(req,res)=>{
+    res.json(restaurants);
+ });
+    router.get('/:id',(req,res)=>{
+    res.send('GET ONE API');
+});
+    router.post('/api/restaurants',(req,res)=>{
+    res.send('POST API');
+});
+    router.put('/:id',(req,res)=>{
+    res.send('PUT API');
+});
+    router.delete(':id',(req,res)=>{
+    res.send('DELETE API');
+});
+    module.exports = router;
